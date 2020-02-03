66338291601d9b49967641a520f94aa616c3baa5
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-03 16:08:36 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,24 @@
 class Transfer
-  
-  def initialize
-    
+  attr_reader :sender, :receiver, :amount
+
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
   end
 
   def valid?
-    
+    self.sender.valid? && self.receiver.valid?
   end
 
   def execute_transaction
-    
+    if self.sender.balance < self.amount
+      puts "You need more money"
+      return nil
+    else
+    self.sender.balance -= amount
+    self.receiver.balance += amount
+    end
   end
 
   def reverse_transfer
